42c9f6d7678b3889e3147e41156150b4ef1a2076
Alberto9701 <beto970154@gmail.com>
2023-11-04 18:47:56 -0600

    agregamos fade in

---
+++ scripts/codigo.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var imagenes = document.querySelectorAll(".fade");
+    var delay = 0;
+  
+    imagenes.forEach(function(imagen) {
+      setTimeout(function() {
+        imagen.classList.add("show");
+      }, delay);
+      delay += 600; // Ajusta la cantidad de milisegundos de retraso entre las imágenes
+    });
+  });
